51699545b9899515a44c0c5ed88144f449ec80e6
ManbirKakkar <->
2025-03-25 21:14:45 -0700

    - Remove unused hilt code

--- base/src/main/java/com/manbirkakkar/base/BaseActivity.kt
+++ base/src/main/java/com/manbirkakkar/base/BaseActivity.kt
@@ -5,11 +5,5 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 
 abstract class BaseActivity : AppCompatActivity() {
-    protected inline fun <reified T : ViewModel> createViewModel(
-        factory: ViewModelProvider.Factory? = null
-    ): T {
-        return factory?.let {
-            ViewModelProvider(this, it)[T::class.java]
-        } ?: ViewModelProvider(this)[T::class.java]
-    }
+
 }
